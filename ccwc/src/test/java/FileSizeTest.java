cd5aafa8573d2a8af6aa7317dca93d4442f3e9d6
PeterAjaaa <89441024+PeterAjaaa@users.noreply.github.com>
2024-06-23 15:56:10 +0700

    Add unit test

---
+++ ccwc/src/test/java/FileSizeTest.java
@@ -0,0 +1,68 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.peterajaaa.WordCount;
+
+import picocli.CommandLine;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class FileSizeTest {
+
+    @Test
+    public void testFileSize() {
+        // Prepare a stream to capture the output from System.out
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(baos);
+
+        // Save the original System.out
+        PrintStream originalOut = System.out;
+
+        // Assign the special stream to System.out
+        System.setOut(ps);
+
+        // Execute the command with the arguments
+        int exitCode = new CommandLine(new WordCount()).execute("-c", "test.txt");
+
+        // Flush the stream
+        ps.flush();
+
+        // Restore the original System.out
+        System.setOut(originalOut);
+
+        // Convert the captured output to a string
+        String output = baos.toString();
+
+        Assertions.assertEquals(0, exitCode);
+        Assertions.assertEquals("342190 test.txt", output.trim());
+    }
+
+    @Test
+    public void testFileNumberOfLines() {
+        // Prepare a stream to capture the output from System.out
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(baos);
+
+        // Save the original System.out
+        PrintStream originalOut = System.out;
+
+        // Assign the special stream to System.out
+        System.setOut(ps);
+
+        // Execute the command with the arguments
+        int exitCode = new CommandLine(new WordCount()).execute("-l", "test.txt");
+
+        // Flush the stream
+        ps.flush();
+
+        // Restore the original System.out
+        System.setOut(originalOut);
+
+        // Convert the captured output to a string
+        String output = baos.toString();
+
+        Assertions.assertEquals(0, exitCode);
+        Assertions.assertEquals("7145 test.txt", output.trim());
+    }
+}
