09aec0edfc331cabb7de6393ba53b2630723db85
PeterAjaaa <89441024+PeterAjaaa@users.noreply.github.com>
2024-06-24 01:24:43 +0700

    Add chars count feature and do some code refactoring

--- ccwc/src/main/java/com/peterajaaa/WordCount.java
+++ ccwc/src/main/java/com/peterajaaa/WordCount.java
@@ -6,7 +6,6 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.Callable;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
@@ -24,48 +23,70 @@ public class WordCount implements Callable<Integer> {
     @Option(names = "-w", description = "Print number of words for the given file")
     boolean countWords;
 
-    @Parameters(paramLabel = "<FILE>", description = "The target file to operate on", arity = "1")
+    @Option(names = "-m", description = "Print number of characters for the given file")
+    boolean countChars;
+
+    @Parameters(paramLabel = "<FILE>", description = "The target file to operate on", arity = "0..1")
     String fileToCount;
 
     @Override
     public Integer call() throws Exception {
-        Path pathToFile = Paths.get(fileToCount);
-        String filename = pathToFile.getFileName().toString();
-        List<String> allLinesFromFile = Files.readAllLines(pathToFile);
-        String templateOutput = "%d %s\n";
+        final Path pathToFile = Paths.get(fileToCount);
+        final String filename = pathToFile.getFileName().toString();
+        final List<String> allLinesFromFile = Files.readAllLines(pathToFile);
+        final String templateOutput = "%d %s\n";
+        final String noFlagTemplateOutput = "%d  %d %d %s\n";
+        final boolean noFlagUsed = !countBytes && !countLines && !countWords && !countChars;
 
         if (!Files.exists(pathToFile)) {
             System.err.println("File " + fileToCount + " does not exist");
             return 1;
-        }
-
-        if (!Files.isReadable(pathToFile)) {
+        } else if (!Files.isRegularFile(pathToFile)) {
+            System.err.println("Path " + fileToCount + " is not a file");
+            return 1;
+        } else if (!Files.isReadable(pathToFile)) {
             System.err.println("File " + fileToCount + " cannot be read");
             return 1;
         }
 
-        if (!Files.isRegularFile(pathToFile)) {
-            System.err.println("Path " + fileToCount + " is not a file");
-            return 1;
-        }
-
-        if (countBytes) {
-            System.out.printf(templateOutput, Files.size(pathToFile), filename);
-        } else if (countLines) {
-            System.out.printf(templateOutput, allLinesFromFile.size(), filename);
-        } else if (countWords) {
+        if (noFlagUsed) {
+            long lineCount = allLinesFromFile.size();
             long wordCount = allLinesFromFile.parallelStream()
                     .map(str -> str.split("\\s+"))
                     .flatMap(Arrays::stream)
                     .filter(item -> !item.isBlank())
                     .count();
-            System.out.printf(templateOutput, wordCount, filename);
+            long bytesCount = Files.size(pathToFile);
+            System.out.printf(noFlagTemplateOutput, lineCount, wordCount, bytesCount, filename);
+        } else {
+            if (countBytes) {
+                long bytesCount = Files.size(pathToFile);
+                System.out.printf(templateOutput, bytesCount, filename);
+            }
+
+            if (countLines) {
+                long lineCount = allLinesFromFile.size();
+                System.out.printf(templateOutput, lineCount, filename);
+            }
+
+            if (countWords) {
+                long wordCount = allLinesFromFile.parallelStream()
+                        .flatMap(str -> Arrays.stream(str.split("\\s+")))
+                        .filter(item -> !item.isBlank())
+                        .count();
+                System.out.printf(templateOutput, wordCount, filename);
+            }
+
+            if (countChars) {
+                long charCount = Files.readString(pathToFile).chars().count();
+                System.out.printf(templateOutput, charCount, filename);
+            }
         }
         return 0;
     }
 
     public static void main(String[] args) {
-        args = new String[] { "-w", "test.txt" };
+        args = new String[] { "" };
         int exitCode = new CommandLine(new WordCount()).execute(args);
         System.exit(exitCode);
     }
